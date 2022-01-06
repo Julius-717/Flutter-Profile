b645966a5347fa84b7fda5421927e5aee2627de3
Julius-717 <waynejulius10@gmail.com>
2022-01-06 21:36:58 +0300

    added title text and color to appbar

--- lib/Pages/About.dart
+++ lib/Pages/About.dart
@@ -8,8 +8,9 @@ class AboutMePage extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Color(0xff040305),
         elevation: 0,
-        
-      )
+        title: Text('About Me', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 25)),
+      ),
+      
     );
   }
 }
