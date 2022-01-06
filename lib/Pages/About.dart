d7ad7e73a12d3e3efdc28e2b3d99657c6ec39ac9
Julius-717 <waynejulius10@gmail.com>
2022-01-06 21:34:22 +0300

    added background color to appbar

---
+++ lib/Pages/About.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AboutMePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff040305),
+      appBar: AppBar(
+        backgroundColor: Color(0xff040305),
+      )
+    );
+  }
+}
